4bc080c9e6d0b7668c850502c202c4a2e28c0e1e
boyer <asboyer@gmail.com>
2021-03-13 19:25:03 -0500

    points sorting

--- scripts/algo.py
+++ scripts/algo.py
@@ -1,20 +1,25 @@
 import json
 
-with open('../stats/stats.json', 'r', encoding='utf8') as file:
-	stats = json.load(file)
-	rankings = {}
+def rank(stats, category):
 	rank = []
 	for player in stats:
-		rank.append([player, float(stats[player]['PTS'])])
+		rank.append([player, float(stats[player][category])])
 	rank = sorted(rank, key=lambda x: x[1])
 	rank.reverse()
 
-	for i in range(len(rank)):
-		name = rank[i][0]
-		points = rank[i][1]
+	return rank
+
+with open('../stats/stats.json', 'r', encoding='utf8') as file:
+	stats = json.load(file)
+	rankings = {}
+	point_rank = rank(stats, 'PTS') 
+
+	for i in range(len(point_rank)):
+		name = point_rank[i][0]
+		points = point_rank[i][1]
 		rankings[name] = {}
 		rankings[name]['PTS Rank'] = i
 		rankings[name]['PTS'] = points
-	print(rankings)
 
-	
+	for player in rankings:
+		print(f"{rankings[player]['PTS Rank'] + 1}. {player}")

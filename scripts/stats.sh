7d8cf11df4df863fb471df123efd84bb0ef86a8f
Andrew Boyer <asboyer@gmail.com>
2021-04-21 09:49:15 -0400

    restructuring

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo Updating standard statistics ...
-python3 stats.py
+python3 stats/stats.py
 echo Updated!
 echo Updating advanced statistics ...
-python3 stats2.py
+python3 stats/stats2.py
 echo Updated!
 

b37a4d5d8789ad6cee665055c4be7f1fe6c65d24
Andrew Boyer <asboyer@gmail.com>
2022-12-07 11:54:03 -0700

    master

--- scripts/stats.py
+++ scripts/stats.py
@@ -2,28 +2,25 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
 
-
 year = 2021
 url = "https://www.basketball-reference.com/leagues/NBA_{}_per_game.html".format(year)
 html = urlopen(url)
 soup = BeautifulSoup(html, 'html.parser')
 
-
-soup.findAll('tr', limit=2)
 headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
 headers = headers[1:]
-
 
 rows = soup.findAll('tr')[1:]
 
 stats = {}
 
 for i in range(len(rows)):
-	if len(rows[i].findAll('td')) > 0:
+	tds = rows[i].findAll('td')
+	if len(tds) > 0:
 		h = 0
-		name = rows[i].findAll('td')[0].getText()
+		name = tds[0].getText()
 		stats[name] = {}
-		for td in rows[i].findAll('td'):
+		for td in tds:
 			stats[name][headers[h]] = td.getText()
 			h += 1
 
